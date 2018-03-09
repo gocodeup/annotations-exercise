930900ed5a393c20ea52ee03654bdf1f6cab1413
TMcCool <tjmccool1@icloud.com>
2018-03-09 11:05:34 -0600

    added supresswarnings

--- src/Author.java
+++ src/Author.java
@@ -15,15 +15,15 @@ public class Author extends Person {
     /**
      * @deprecated Use publishedBooks instead
      */
-    @Deprecated
+    @Deprecated @SuppressWarnings("unchecked")
     public List<String> getBooks() {
         return books;
     }
-
+    @SuppressWarnings("unchecked")
     public List<String> publishedBooks() {
         return books;
     }
-
+    @SuppressWarnings("unchecked")
     public void addBook(String book) {
         books.add(book);
     }
