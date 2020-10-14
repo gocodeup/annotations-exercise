4be261e15060d2d00d1aed436940575215daa790
Donelle Harris <donelle.harris@gmail.com>
2020-10-14 11:47:54 -0500

    Fix javac errors

--- src/Author.java
+++ src/Author.java
@@ -5,11 +5,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Author extends Person {
-    private List books;
+    private List<String> books;
 
     public Author(String firstName, String lastName) {
         super(firstName, lastName);
-        books = new ArrayList();
+        books = new ArrayList<String>();
     }
 
     /**
@@ -28,8 +28,8 @@ public class Author extends Person {
         books.add(book);
     }
 
-    @Override
     public String sortName() {
         return String.format("%s, %s", lastName, firstName);
     }
+
 }
