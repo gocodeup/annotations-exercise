4ca2caac5276ed93339791da1a045132491e026c
Michael Satterfield <michael.f.satterfield@gmail.com>
2020-10-14 11:49:30 -0500

    fixed error using a under the rug method

--- src/Author.java
+++ src/Author.java
@@ -28,7 +28,7 @@ public class Author extends Person {
         books.add(book);
     }
 
-    @Override
+
     public String sortName() {
         return String.format("%s, %s", lastName, firstName);
     }
