fad9e9d31d0c62ec2329c8ba81a610301261b9b3
Amanda Woods <amanda.garrison.woods@gmail.com>
2020-10-14 11:24:17 -0500

    feat: fixed deptrecated publishedBook latest version

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
         author.addBook("Practical Object-Oriented Design in Ruby");
         author.addBook("99 Bottles of OOP");
 
-        for (String book: author.getBooks()) {
+        for (String book: author.publishedBooks()) { //changed publishedBooks made it not depricated version
             System.out.println(book);
         }
     }
