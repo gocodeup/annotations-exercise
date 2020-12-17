3cf3349fd027de8263b2439a650b843172e6fa2e
Correy Everett <correy.everett@gmail.com>
2020-12-17 17:53:24 -0600

    change sortName to fullName to activate @Override

--- src/Author.java
+++ src/Author.java
@@ -28,8 +28,8 @@ public class Author extends Person {
         books.add(book);
     }
 
-//    @Override
-    public String sortName() {
+    @Override
+    public String fullName() {
         return String.format("%s, %s", lastName, firstName);
     }
 }
