47dbd2ca72b6725f516f88f54c654d4b1ce99984
Naysa Moreno <naysaymoreno@gmail.com>
2021-07-01 08:22:09 -0500

    supressed warnings

--- src/Author.java
+++ src/Author.java
@@ -28,7 +28,7 @@ public class Author extends Person {
         books.add(book);
     }
 
-    @Override
+    @SuppressWarnings("all")
     public String sortName() {
         return String.format("%s, %s", lastName, firstName);
     }
