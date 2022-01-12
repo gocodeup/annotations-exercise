f50fcee80f1eea27b0fde0402b939e86692c262d
Nicholas <nicholasdiazjr509@gmail.com>
2022-01-12 11:46:23 -0600

    finished better_fix

--- src/Author.java
+++ src/Author.java
@@ -4,7 +4,7 @@
 import java.util.ArrayList;
 import java.util.List;
 
-@SuppressWarnings({"unchecked", "deprecation"})
+//@SuppressWarnings({"unchecked", "deprecation"})
 public class Author extends Person {
     private List books;
 
